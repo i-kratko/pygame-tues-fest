8b2948e3563bd6b14ae2003010dc2ad755dbd983
i-kratko <66026762+i-kratko@users.noreply.github.com>
2022-03-30 17:37:19 +0300

    StateManager

--- Code/main.py
+++ Code/main.py
@@ -3,22 +3,20 @@ import pygame
 from sys import exit
 
 
-#innit
-pygame.init()
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Test')
-clock = pygame.time.Clock()
+
 
 class StateManager():
     def __init__(self):
         self.level = 1
 
     #TODO: adekvatno ime
-    def start():
+    def start(self):
+        gameOver = False
+
         test_surface = pygame.Surface((200,300))
         test_surface.fill('Blue')
 
-        while True:
+        while not gameOver:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -31,10 +29,20 @@ class StateManager():
 
     def stateManager(self):
         if self.level == 1:
-            
+            self.start()
 
-
+#innit
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Test')
+clock = pygame.time.Clock()
+     
+stateManager = StateManager()
 
 #gameloop
 def gameLoop():
+    while True:
+        stateManager.stateManager()
+
+gameLoop()
     
