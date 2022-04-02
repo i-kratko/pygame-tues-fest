ee9869bc19ac79c71365370683a11082cfe7af4b
i-kratko <66026762+i-kratko@users.noreply.github.com>
2022-04-02 23:00:23 +0300

    Fixed main menu

--- Code/main.py
+++ Code/main.py
@@ -6,7 +6,7 @@ from button import Button
 
 
 def get_font(size): # Returns Press-Start-2P in the desired size
-        return pygame.font.Font("assets/font.ttf", size)
+        return pygame.font.Font("Graphics/font.ttf", size)
 
 
 #bruh
@@ -27,11 +27,11 @@ class StateManager():
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(pos=(640, 250), text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(pos=(640, 400), text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(pos=(640, 550), text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        PLAY_BUTTON = Button(None, pos=(640, 250), text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(None, pos=(640, 400), text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(None, pos=(640, 550), text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
-        display.blit(MENU_TEXT, MENU_RECT)
+        display.blit(MENU_TEXT, (0, 0))
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
@@ -53,6 +53,9 @@ class StateManager():
                         self.stateManager()
 
             display.blit(bgSurface,(0,0))
+            display.blit(PLAY_BUTTON.image, (230, 50))
+            display.blit(OPTIONS_BUTTON.image, (125, 150))
+            display.blit(QUIT_BUTTON.image, (230, 250))
 
             pygame.display.update()
             clock.tick(const.FPS)
