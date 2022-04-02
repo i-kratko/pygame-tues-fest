e861277c9c11a6ffd2cbc6c6a38d3301d0ab9ba7
bigIq <viktorio.k.milanov.2019@elsys-bg.org>
2022-04-02 22:49:40 +0300

    bruh

--- Code/main.py
+++ Code/main.py
@@ -2,13 +2,18 @@ import pygame
 from sys import exit
 import const
 from player import Player
+from button import Button
+
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+        return pygame.font.Font("assets/font.ttf", size)
 
 
 #bruh
 class StateManager():
+
     def __init__(self):
         self.level = 1
-
 
     #TODO: main menu
     def mainMenu(self):
@@ -16,6 +21,26 @@ class StateManager():
 
         bgSurface = pygame.Surface((800,600))
         bgSurface.fill('Blue')
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(pos=(640, 250), text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(pos=(640, 400), text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(pos=(640, 550), text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        display.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(display)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
         while not gameOver:
             for event in pygame.event.get():
@@ -100,4 +125,3 @@ def gameLoop():
         stateManager.stateManager()
 
 gameLoop()
-    
